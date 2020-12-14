d6b6623469614b558ab5505c5acb3641fe0c69d4
Abrar Akhand <Jahin345@gmail.com>
2020-12-13 22:04:35 -0700

    WPE uploaded

---
+++ WPE Snow App/Assets/StarParticles.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarParticles : MonoBehaviour
+{
+
+    [SerializeField] private ParticleSystem starParticles; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (starParticles != null)
+        {
+            starParticles.Stop();
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if (starParticles != null)
+        {
+            starParticles.Play();
+            //Debug.Log("Mouse Entered");
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (starParticles != null)
+        {
+            starParticles.Stop();
+            //Debug.Log("Mouse LEFT");
+        }
+    }
+}
